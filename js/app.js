ae09a49764bf9663f14ce5d8b68f8fdfdfb440fe
Astrid Paris <astrid@astridparis.com>
2015-05-22 17:33:51 -0400

    Added js for donuts

--- js/app.js
+++ js/app.js
@@ -31,3 +31,49 @@ var runTotal = $("#runTotal").get(0).getContext("2d");
 
 //This will get the first returned node in the jQuery collection.
 var myLineChart = new Chart(runTotal).Line(weeklyRunTotals, chartOptions);
+
+
+// Doughnut Chart for Weekly Goals
+var weeklyGoal = [
+  {
+    value: 10,
+    color:"#4eddc1"
+  },
+  {
+    value : 90,
+    color : "#E2EAE9"
+  }
+]
+var options = {
+  percentageInnerCutout : 75
+};
+var thisWeek = $("#weeklyGoal").get(0).getContext("2d");
+var myWeeklyGoal = new Chart(thisWeek).Doughnut(weeklyGoal, options);
+
+// Doughnut Chart for Monthly Goals
+var monthlyGoal = [
+  {
+    value: 95,
+    color:"#1abc9c"
+  },
+  {
+    value : 5,
+    color : "#E2EAE9"
+  }
+]
+var thisMonth = $("#monthlyGoal").get(0).getContext("2d");
+var myMonthlyGoal = new Chart(thisMonth).Doughnut(monthlyGoal, options);
+
+// Doughnut Chart for Annual Goals
+var annualGoal = [
+  {
+    value: 72,
+    color:"#0b9277",
+  },
+  {
+    value : 28,
+    color : "#E2EAE9"
+  }
+]
+var thisYear = $("#annualGoal").get(0).getContext("2d");
+var myAnnualGoal = new Chart(thisYear).Doughnut(annualGoal, options);
